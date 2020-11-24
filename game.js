b6ca6787ca7c94a5846e869481f242a141415733
Joe Adamo <josephaadamo67@gmail.com>
2020-11-24 17:22:05 -0600

    Basic Responsive Design implemented. Site and game are fully functional. To-Do link to personal site and further improves media queries.

--- game.js
+++ game.js
@@ -1,17 +1,16 @@
-
 let options = ["rock", "paper", "scissors"]
 
 let optionsURL = ["./images/icon.rock.svg", "./images/icon.paper.svg", "./images/icon.scissors.svg"]
 
 let computerChoice = "";
 let userChoice = "";
-let userChoiceNumber ="";
+let userChoiceNumber = "";
 let gameResult = "";
 let score = 0;
 
-
+//handles the game
 $(".button").click(function() {
-    
+
     userChoiceNumber = options.indexOf($(this).attr("id"));
     userChoice = $(this).attr("id");
     computerChoice = computersPick();
@@ -20,21 +19,21 @@ $(".button").click(function() {
 
     $(".choiceSelection").addClass("invisible");
     $(".resultsContainer").removeClass("invisible");
-    
-    $("#user").attr("src", "images/icon-"+ userChoice +".svg")
+
+    $("#user").attr("src", "images/icon-" + userChoice + ".svg")
     $("#player").addClass(userChoice);
     $("#player").addClass("button");
 
-    $("#machine").attr("src", "images/icon-"+ options[computerChoice] +".svg")
+    $("#machine").attr("src", "images/icon-" + options[computerChoice] + ".svg")
     $("#computer").addClass(options[computerChoice]);
     $("#computer").addClass("button");
 
     $("#status").text(gameResult);
     $(".value").text(score);
-//TODO: Show Side By Side. Edit middle text and create a play again button.
 
 });
 
+//resets the game besides score
 $(".playAgain").click(function() {
     $(".resultsContainer").addClass("invisible");
     $(".choiceSelection").removeClass("invisible");
@@ -45,17 +44,17 @@ $(".playAgain").click(function() {
 });
 
 
-function computersPick(){
+function computersPick() {
     return Math.floor(Math.random() * 3);
 }
-
-function compare(choice1, choice2){
+//compares two numbers to see which is greater/check for scissors(2) vs rock(0) condition
+function compare(choice1, choice2) {
     console.log(choice1);
     console.log(choice2);
 
     if (choice1 == choice2) {
         return "It's a Tie!";
-        
+
     }
     if (choice1 == options.length - 1 && choice2 == 0) {
         score--;
@@ -74,12 +73,11 @@ function compare(choice1, choice2){
     }
 
 }
-//TODO: Rules Modal Popup.
-
-$(".rulesButton").on("click", function(){
+//rules modal
+$(".rulesButton").on("click", function() {
     $(".rulesOverlay, .rulesContainer").addClass("active");
 });
 
 $(".closeButton, .rulesOverlay").on("click", function() {
     $(".rulesOverlay, .rulesContainer").removeClass("active");
-  });
+});
