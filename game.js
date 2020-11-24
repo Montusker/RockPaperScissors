5155ab09162ff8cbb170165245704faa5288ec2d
Joe Adamo <josephaadamo67@gmail.com>
2020-11-24 15:23:44 -0600

    Finished J. TO-DO: Final style check and responsive design

--- game.js
+++ game.js
@@ -1,46 +1,85 @@
 
-let options = ["Rock", "Paper", "Scissors"]
+let options = ["rock", "paper", "scissors"]
 
 let optionsURL = ["./images/icon.rock.svg", "./images/icon.paper.svg", "./images/icon.scissors.svg"]
 
-let computerChoice = ""
+let computerChoice = "";
 let userChoice = "";
+let userChoiceNumber ="";
+let gameResult = "";
 let score = 0;
-$(".button").click(function() {
-    userChoice = options.indexOf($(this).attr("id"));
-    console.log(userChoice);
-    computerChoice = computersPick();
-    console.log(computerChoice);
-    console.log(compare(userChoice, computerChoice));
 
+
+$(".button").click(function() {
+    
+    userChoiceNumber = options.indexOf($(this).attr("id"));
+    userChoice = $(this).attr("id");
+    computerChoice = computersPick();
+    gameResult = compare(userChoiceNumber, computerChoice);
+
+
+    $(".choiceSelection").addClass("invisible");
+    $(".resultsContainer").removeClass("invisible");
+    
+    $("#user").attr("src", "images/icon-"+ userChoice +".svg")
+    $("#player").addClass(userChoice);
+    $("#player").addClass("button");
+
+    $("#machine").attr("src", "images/icon-"+ options[computerChoice] +".svg")
+    $("#computer").addClass(options[computerChoice]);
+    $("#computer").addClass("button");
+
+    $("#status").text(gameResult);
+    $(".value").text(score);
 //TODO: Show Side By Side. Edit middle text and create a play again button.
 
+});
+
+$(".playAgain").click(function() {
+    $(".resultsContainer").addClass("invisible");
+    $(".choiceSelection").removeClass("invisible");
+    $("#player").removeClass(userChoice);
+    $("#player").removeClass(userChoice);
+    $("#computer").removeClass(options[computerChoice]);
+    $("#computer").removeClass("button");
 });
 
 
 function computersPick(){
     return Math.floor(Math.random() * 3);
 }
+
 function compare(choice1, choice2){
+    console.log(choice1);
+    console.log(choice2);
+
     if (choice1 == choice2) {
-        return "Tie";
+        return "It's a Tie!";
+        
     }
     if (choice1 == options.length - 1 && choice2 == 0) {
         score--;
-        return "Right wins";
+        return "The Computer Won!";
     }
     if (choice2 == options.length - 1 && choice1 == 0) {
         score++;
-        return "Left wins";
+        return "You Won!!!";
     }
     if (choice1 > choice2) {
         score++;
-        return "Left wins";
+        return "You Won!!!";
     } else {
         score--;
-        return "Right wins";
+        return "The Computer Won!";
     }
 
 }
 //TODO: Rules Modal Popup.
 
+$(".rulesButton").on("click", function(){
+    $(".rulesOverlay, .rulesContainer").addClass("active");
+});
+
+$(".closeButton, .rulesOverlay").on("click", function() {
+    $(".rulesOverlay, .rulesContainer").removeClass("active");
+  });
